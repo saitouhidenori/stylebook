0268037283bd780b68e29e723db7a885e281e4b0
saitou hidenori <saitouhidenori@MacBook-Pro-saito.local>
2016-03-05 16:55:54 +0900

    user.mypage.like多い順にdesigner出力,下部にdesigner profile掲載編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,19 @@ class UsersController < ApplicationController
 
   def show
      @styles = current_user.likes.map{ |like| like.style }
-    #  designers = styles.group_by{ |style| style.designer_id }
-    #  designers.inject([]) do |arr, (key, val)|
-    #   arr << { "#{key}" => val.count }
-    #   arr
-    # end
+
+     styles = current_user.likes.map{ |like| like.style }
+     designers = styles.group_by{ |style| style.designer_id }
+     arr = designers.inject([]) do |arr, (key, val)|
+        arr << { key => val.count }
+     end
+     @designers = arr.sort { |a, b| b.values[0] <=> a.values[0] }.map{|i| i.keys[0]}.map{|i| Designer.find(i)}
+
+    # current_user.(params[id]).like.class
+
+    # Style.all.sort_by{|style|style.likes.count}
     # @user = current_user
     # @likes = Like.find_by(user_id: @user.id)
     # @styles = Style.where(id: @style.id)
+    end
   end
-end
